20a7e38009177a6e327b0ae5c7493f6508545c0a
lostmuchbit <67940731+lostmuchbit@users.noreply.github.com>
2020-07-08 10:04:13 +0800

    Update 欧几里得算法求最大公约数.cpp
    为中文描述更具体而修改

--- 欧几里得算法求最大公约数.cpp
+++ 欧几里得算法求最大公约数.cpp
@@ -1,30 +1,32 @@
+/*总的来说没什么问题，可能就是不记得欧几里得算法是什么了*/
 #include "stdio.h"
 
 int main()
 {
-	   int a,b,c,r;
-   	scanf("%d%d",&a,&b);
+	int a,b,c,r;
+	scanf("%d%d",&a,&b);//输入两个数
 	
-	   if(a>=b)
-   	{
-       while(a%b!=0)
-	       {
-		          r=a%b;
-		          a=b;
-		          b=r;
-	       }
-   	}
-   	else
-   	{
-       		c=a;a=b;b=c;
-	       while(a%b!=0)
-	       {
-		          r=a%b;
-		          a=b;
-		          b=r;
-	       } 
-   	}
+	if(a>=b)//比较a,b大小
+	{
+            while(a%b!=0)//只要a除以b有余数就继续进行
+	   {
+		  r=a%b;//欧几里得算法内容
+		  a=b;
+		  b=r;
+	   }
+	}
+	else
+	{
+	   c=a;a=b;b=c;//让大的数为a，小的数为b
+	   while(a%b!=0)
+	   {
+		  r=a%b;
+		  a=b;
+		  b=r;
+	   } 
+	}
 	
-	   printf("%d",r);
-	   return 0;
-	} 
+	printf("%d",r);//输出最大公约数
+	return 0;
+	
+} 
