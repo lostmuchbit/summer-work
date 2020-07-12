cd33632d67b70a01a0fcdc459ee437f3aa921ae9
lostmuchbit <67940731+lostmuchbit@users.noreply.github.com>
2020-07-12 17:44:05 +0800

    Create 0~n-1唯一缺少的数.c
    创建0~n-1唯一缺少的数.c

---
+++ 0~n-1唯一缺少的数.c
@@ -0,0 +1,29 @@
+#include "stdio.h"
+#define Max 10000
+
+int missingNumber(int nums[], int numsSize)
+{
+    int i=0;
+    for(i=0;i<numsSize;i++)
+    {
+        if(nums[i]!=i)
+        {
+            return i;
+            break;
+        }
+    }
+}
+
+int main()
+{
+    int nums[Max],numsSize;
+    printf("输入字符串长度:");
+    scanf("%d",&numsSize);
+
+    printf("输入字符串:");
+    for(int i=0;i<numsSize;i++)
+    scanf("%d",&nums[i]);
+
+    printf("%d",missingNumber(nums,numsSize));
+    return 0;
+}
